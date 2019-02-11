cc42a40f0bb8c717199a779abfe7fee8a6ae2350
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-02-10 22:05:12 -0200

    Resolution Regression

--- ud120_projects/regression/finance_regression.py
+++ ud120_projects/regression/finance_regression.py
@@ -17,7 +17,10 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+
+arquivo = open("../final_project/final_project_dataset_modified.pkl", "rb")
+dictionary = pickle.load(arquivo)
+arquivo.close()
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -26,7 +29,7 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
 test_color = "b"
@@ -37,13 +40,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
-
+print ("Slope:", reg.coef_)
+print ("Intercept:", reg.intercept_)
+print ("Score Training:", reg.score(feature_train, target_train))
+print ("Score Testing:", reg.score(feature_test, target_test))
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
