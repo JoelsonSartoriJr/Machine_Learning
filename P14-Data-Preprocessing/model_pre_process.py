e942bf3662b2d4bb50dbc909d910234074bf9c50
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-05-16 15:51:34 -0300

    Data Preprocessing

---
+++ P14-Data-Preprocessing/model_pre_process.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+#Import libs
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import dataset
+dataset = "/home/joelson/Github/Machine_Learning/P14-Data-Preprocessing/Data_Preprocessing/Data.csv"
+df = pd.read_csv(dataset)
+X = df.iloc[:, :-1].values
+y = df.iloc[:, 3].values
+
+#Missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_X.fit_transform(y)
+
+#Splitting the Dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
