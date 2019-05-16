e1dde650d38b16edb209dff242d56f77602eeb4a
Joelson Sartori Junior <joelsonsartori@gmail.com>
2019-05-16 16:55:27 -0300

    Data Preprocessing

--- P14-Data-Preprocessing/model_pre_process.py
+++ P14-Data-Preprocessing/model_pre_process.py
@@ -33,3 +33,9 @@ y = labelEncoder_X.fit_transform(y)
 #Splitting the Dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#FeaturesScaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
